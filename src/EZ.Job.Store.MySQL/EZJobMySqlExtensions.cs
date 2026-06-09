3b619b174f945a321cf6d106aa0cb72a78b6ed3a
Gabriel Albuquerque <gabriel@example.com>
2026-06-09 10:43:21 -0300

    feat: migrate to IEZJobBuilder, update schema with started_at/completed_at, bump v2.0.0

--- src/EZ.Job.Store.MySQL/EZJobMySqlExtensions.cs
+++ src/EZ.Job.Store.MySQL/EZJobMySqlExtensions.cs
@@ -5,12 +5,12 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class EZJobMySqlExtensions
 {
-    public static EZJobBuilder AddMySqlStore(this EZJobBuilder builder, string connectionString)
+    public static IEZJobBuilder AddMySqlStore(this IEZJobBuilder builder, string connectionString)
     {
         return AddMySqlStore(builder, o => o.ConnectionString = connectionString);
     }
 
-    public static EZJobBuilder AddMySqlStore(this EZJobBuilder builder, Action<MySqlStoreOptions> configure)
+    public static IEZJobBuilder AddMySqlStore(this IEZJobBuilder builder, Action<MySqlStoreOptions> configure)
     {
         var options = new MySqlStoreOptions();
         configure(options);
